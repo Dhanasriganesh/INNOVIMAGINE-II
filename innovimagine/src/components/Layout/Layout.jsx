a9f7461b36c23aabc471a786710fe8ba0dffd6b1
dhanasriganesh <dhanasriganesh9@gmail.com>
2025-02-20 10:40:38 +0530

    basic

--- innovimagine/src/components/Layout/Layout.jsx
+++ innovimagine/src/components/Layout/Layout.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Routers from '../Routers/Routers'
+import Herosection from '../comp/Herosection'
 const Layout = () => {
   return (
     <Router>
-      <Header/>
-      <Routers/>
-      <Footer/>
+      {/* <Header/> */}
+<Herosection/>
+      {/* <Routers/> */}
+      {/* <Footer/> */}
     </Router>
   )
 }
